3221b7a520ab2014127dafee53e1b097a71150dd
Cheikh Tidiane Ndiaye <cheikhtiindiaye@gmail.com>
2022-09-11 03:48:37 +0100

    fns

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,14 +36,56 @@ $('.ess-carroussel-container').slick({
 }
 );
 
-// Filtre button 
-$(function () {
-  $('.show-filtre').click(function(){
-    $('.catalogue-aside ').addClass('d-block');
-  });
-  $(document).on("click", function (e) {
-      if ($(e.target).is("#user-login-wrapper") === false) {
-          $("#user-login-wrapper").removeClass("wide");
+// Exclusives Shops Carroussel
+$('.products-related-carroussel').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  prevArrow: '#button-rp-left',
+  nextArrow: '#button-rp-right',
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
       }
-  });
+    },
+    {
+      breakpoint: 560,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+}
+);
+
+// Filtre button 
+$('.show-filtre').click(function(){
+  $('.nav-closer').removeClass('d-none');
+  $('.catalogue-aside').removeClass('d-none');
+});
+
+// Product Image Change 
+$('.single-product-section .container .row .product-details .display-images .di-left img').click(function(){
+  var $smallImage = $(this).attr('src');
+  $('#mainImage').attr('src', $smallImage);
+});
+
+// Hide Aside
+$('.nav-closer').click(function(){
+  $('.nav-closer').addClass('d-none');
+  $('.catalogue-aside').addClass('d-none');
+  $('.single-product-aside').addClass('d-none');
+})
+
+// Filtre button 
+$('.show-shop-details').click(function(){
+  // alert('ok')
+  $('.nav-closer').removeClass('d-none');
+  $('.single-product-aside').removeClass('d-none');
+  // $('.catalogue-aside').removeClass('d-none');
 });
